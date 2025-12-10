1aeb2d9d446c18e13cfe7aecb450935128aa7241
Maxence Roux <maxence.b.roux@gmail.com>
2025-12-10 11:33:32 +0100

    Fix username

--- frontend/src/hooks/useWizardConfig.ts
+++ frontend/src/hooks/useWizardConfig.ts
@@ -11,6 +11,7 @@ const defaultConfig: WizardConfiguration = {
     serverUrl: '',
     apiKey: '',
     baseDomain: 'headscale.local',
+    serverVpnHostname: '',
   },
   navidrome: {
     enabled: false,
